1b7d4963c16159deb938932a42371984534530da
Quojo_Steve <quojosteve@gmail.com>
2023-05-24 01:44:25 +0000

    first commit

---
+++ Amonic Airlines/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Amonic_Airlines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No logout detected for your login on";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Reason";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 107);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(477, 136);
+            this.textBox1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 251);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(347, 46);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(397, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Confirm";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(147, 22);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Software crash";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(191, 16);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(135, 22);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "System crash";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 309);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximumSize = new System.Drawing.Size(1000, 500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "No logout detected";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Amonic Airlines/Form2.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Amonic_Airlines
+{
+    public partial class Form2 : Form
+    {
+        public string connection = "Data Source=MONSTER\\SQLEXPRESS;Initial Catalog=Session_1;Integrated Security=True";
+        public static DataTable dt = new DataTable();
+        public static DataTable reason = new DataTable();
+        System.Timers.Timer timer;
+
+
+        int count = 0;
+        private int secondsRemaining = 600; // 10 minutes
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            string newpassword = GetMd5Hash(password);
+            if (username == "" || password == "")
+            {
+                this.Hide();
+                MessageBox.Show("Please enter your username and password");
+                this.Show();
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection logconnection = new SqlConnection(connection);
+                    SqlCommand sqlCommand = new SqlCommand("select * from [dbo].[Users] where @username = [dbo].[Users].[Email]" +
+                        " and @password = [dbo].[Users].[Password]", logconnection);
+                    sqlCommand.Parameters.AddWithValue("@username", username);
+                    sqlCommand.Parameters.AddWithValue("@password", newpassword);
+                    SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+
+                    adapter.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        int id = Convert.ToInt32(dt.Rows[0]["ID"]);
+                        DateTime currenttime = DateTime.Now;
+                        //DateTime utctime = DateTime.UtcNow;
+                        try
+                        {
+                            SqlConnection conn = new SqlConnection(connection);
+                            SqlCommand cmd = new SqlCommand("insert into [dbo].[reasons]([dbo].[reasons].[UserID],[dbo].[reasons].[logon])" +
+                                "values(@userid,@logon)", logconnection);
+                            cmd.Parameters.AddWithValue("@userid", id);
+                            cmd.Parameters.AddWithValue("@logon", currenttime);
+                            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                            dataAdapter.Fill(reason);
+                            Console.WriteLine(currenttime);
+                            
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("" + ex);
+                        }
+
+                        Form1 f = new Form1();
+                        this.Hide();
+                        f.Show();
+                    }
+                    else
+                    {
+                        count++;
+                        if (count == 3)
+                        {
+                            countdown();
+                            count = 1;
+                        }
+                        else
+                        {
+                            this.Hide();
+                            MessageBox.Show("Either your password or username is incorrect");
+                            this.Show();
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("q" + ex);
+                }
+            }
+
+
+        }
+        private void countdown()
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+            timer.Enabled = true;
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+
+        }
+
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+
+                secondsRemaining--;
+
+                if (secondsRemaining <= 0)
+                {
+                    var timer = (System.Timers.Timer)source;
+                    timer.Stop();
+                    timer.Enabled = false;
+                    button1.Enabled = true;
+                    button1.Text = "Login";
+                    secondsRemaining = 60;
+                    label6.Text = "";
+                    return;
+                }
+
+
+                var minutes = secondsRemaining / 60;
+                var seconds = secondsRemaining % 60;
+                String countText = string.Format("{0}:{1}", minutes.ToString().PadLeft(2, '0'), seconds.ToString().PadLeft(2, '0'));
+                button1.Text = countText;
+                label6.Text = string.Format("Invalid login credentials, try again in {0}:{1}", minutes.ToString().PadLeft(2, '0'), seconds.ToString().PadLeft(2, '0'));
+                label6.Visible = true;
+            }));
+        }
+
+        public static string GetMd5Hash(string input)
+        {
+            // Create a new instance of the MD5CryptoServiceProvider class
+            using (MD5 md5Hash = MD5.Create())
+            {
+                // Convert the input string to a byte array
+                byte[] data = md5Hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input));
+
+                // Create a new StringBuilder to collect the bytes
+                System.Text.StringBuilder sBuilder = new System.Text.StringBuilder();
+
+                // Loop through each byte of the hashed data and format each one as a hexadecimal string
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                // Return the hexadecimal string
+                return sBuilder.ToString();
+            }
+        }
+    }
+}
